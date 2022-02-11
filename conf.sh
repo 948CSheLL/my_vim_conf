fb71f36bdaf309980d7bc84d0c0797618ebf0b9b
948CSheLL <2418942810@qq.com>
2022-02-11 02:16:56 -0800

    modify

--- conf.sh
+++ conf.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-function logit() {
-  exit_status=$?
-  echo "[`date`] - ${1}" | tee -a ${LOG_FILE}
-}
-
-function exec_command() {
-  logit "command: ${1} ............................................ running."
-  logit $(${1})
-  if [ ${exit_status} -ne 0 ]; then
-  logit "command: ${1} ............................................ error!"
-    exit ${exit_status}
-  fi 
-  logit "command: ${1} ............................................ done!"
-}
+source ./utils.sh
 
 login_user=$(who -u | cut -d' ' -f1)
+
 login_user_home=$(cat /etc/passwd | grep ${login_user} | cut -d':' -f6)
+
 LOG_FILE="install.log"
+
 exit_status=0
 
 # following env for [go get] command
 export GOPROXY=https://goproxy.io
+
 export GO111MODULE=on
+
 exec_command "cd ${login_user_home}/.vim/pack/minpac/start/YouCompleteMe"
+
 exec_command "python3 install.py --all"
-logit "all done!!!"
+
+logit "all done!!!" "log"
