c538797447926a6d28b25500e75605d38596c7a5
948CSheLL <2418942810@qq.com>
2022-02-10 19:59:49 -0800

    modify

--- conf.sh
+++ conf.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+function logit() {
+  LOG_FILE="install.log"
+  echo "[`date`] - ${1}" | tee -a ${LOG_FILE}
+}
+
 function exec_command() {
-  echo "command: ${1} ............................................ running."
-  ${1}
+  logit "command: ${1} ............................................ running."
+  logit $(${1})
   exit_status=$?
   if [ $(($exit_status)) -ne 0 ]; then
-  echo "command: ${1} ............................................ error!"
+  logit "command: ${1} ............................................ error!"
     exit $(($exit_status))
   fi 
-  echo "command: ${1} ............................................ done!"
+  logit "command: ${1} ............................................ done!"
 }
 
 # following env for [go get] command
@@ -16,4 +21,4 @@ export GOPROXY=https://goproxy.io
 export GO111MODULE=on
 exec_command "cd $HOME/.vim/pack/minpac/start/YouCompleteMe"
 exec_command "python3 install.py --all"
-echo "all done!!!"
+logit "all done!!!"
