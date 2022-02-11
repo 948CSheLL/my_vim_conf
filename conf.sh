bb18bc3fd2dfdb082ab6c5f975cdb840dffd2f41
948CSheLL <2418942810@qq.com>
2022-02-11 05:16:47 -0800

    modify

--- conf.sh
+++ conf.sh
@@ -2,14 +2,6 @@
 
 source ./utils.sh
 
-login_user=$(who -u | cut -d' ' -f1)
-
-login_user_home=$(cat /etc/passwd | grep ${login_user} | cut -d':' -f6)
-
-LOG_FILE="install.log"
-
-exit_status=0
-
 # following env for [go get] command
 export GOPROXY=https://goproxy.io
 
@@ -18,5 +10,7 @@ export GO111MODULE=on
 exec_command "cd ${login_user_home}/.vim/pack/minpac/start/YouCompleteMe"
 
 exec_command "python3 install.py --all"
+
+exec_command "cd -"
 
 logit "all done!!!" "log"
