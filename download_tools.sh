f36f44ece1f749a668733c59849a6fba2a62826a
948CSheLL <2418942810@qq.com>
2022-02-12 06:54:51 -0800

    modify

--- download_tools.sh
+++ download_tools.sh
@@ -4,11 +4,11 @@ source ./utils.sh
 
 (
 
-  install_minpac ${LOGIN_USER} ${LOGIN_USER_HOME}
+  install_minpac "${LOGIN_USER}" "${LOGIN_USER_HOME}"
 
-  install_other_plugins ${LOGIN_USER} ${LOGIN_USER_HOME}
+  install_other_plugins "${LOGIN_USER}" "${LOGIN_USER_HOME}"
 
-  install_ycm ${LOGIN_USER} ${LOGIN_USER_HOME}
+  install_ycm "${LOGIN_USER}" "${LOGIN_USER_HOME}"
 
   exec_command "chown -R ${LOGIN_USER}:${LOGIN_USER} ${LOGIN_USER_HOME}/.vim" "${CMD_CHOWN}"
 
