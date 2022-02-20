c6d6dc392a4783848adbfc92e1c6b0fe8d1ad896
948CSheLL <2418942810@qq.com>
2022-02-20 06:30:52 -0800

    modify download_tools.sh

--- download_tools.sh
+++ download_tools.sh
@@ -8,8 +8,6 @@ source ./utils.sh
 
   install_other_plugins "${LOGIN_USER}" "${LOGIN_USER_HOME}"
 
-  install_ycm "${LOGIN_USER}" "${LOGIN_USER_HOME}"
-
   exec_command "chown -R ${LOGIN_USER}:${LOGIN_USER} ${LOGIN_USER_HOME}/.vim" "${CMD_CHOWN}"
 
 ) &
